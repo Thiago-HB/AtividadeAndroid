430e0038fe63e95e2f75e314870dd14340dea1eb
ANTONIO PARIS MORAIS JUNIOR <200031338@ucsal.br>
2025-09-10 12:09:56 -0300

    Inicializando projeto

---
+++ app/src/main/java/com/example/atividade/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.atividade;
+
+import static com.example.atividade.R.id.ToolbarTela1;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
